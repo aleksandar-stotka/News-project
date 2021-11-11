4358c9a275e62038c4114b14b67a180e414a77c6
aleksandar angelevski <stotka123@yahoo.com>
2021-11-11 11:37:38 +0100

    search-form-done

--- src/context.js
+++ src/context.js
@@ -38,14 +38,17 @@ const AppProvider = ({ children }) => {
   const removeStory = (id) => {
     dispatch({ type: REMOVE_STORY, payload: id });
   };
+  const handleSearch = (query) => {
+    dispatch({ type: HANDLE_SEARCH, payload: query });
+  };
 
   useEffect(() => {
     fetchStories(`${API_ENDPOINT}query=${state.query}&page=${state.page}`);
-  }, []);
+  }, [state.query]);
 
   ////////////
   return (
-    <AppContext.Provider value={{ ...state, removeStory }}>
+    <AppContext.Provider value={{ ...state, removeStory, handleSearch }}>
       {children}
     </AppContext.Provider>
   );
--- src/reducer.js
+++ src/reducer.js
@@ -22,6 +22,13 @@ const reducer = (state, action) => {
         ...state,
         hits: state.hits.filter((story) => story.objectID !== action.payload),
       };
+
+    case HANDLE_SEARCH:
+      return {
+        ...state,
+        query: action.payload,
+        page: 0,
+      };
     default:
       throw new Error(`no mathcing "${action.type}" action type`);
   }
